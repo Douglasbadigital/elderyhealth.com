0857c1f266e821246afdfb3ca57a1b2d06c563de
Douglasbadigital <douglasbadigital@gmail.com>
2026-04-09 11:14:02 -0300

    Update script.js

--- vsl/script.js
+++ vsl/script.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // ⏱️ Tempo de Delay: Formato "Minutos:Segundos"
     // Exemplo: "48:55", "12:00" ou "00:15"
-    tempoDeDelay: "00:00",
+    tempoDeDelay: "00:15",
 
     // 🔗 Links de Checkout dos Botões (Men Balance Pro)
     linkPote2: "/b?p=MBP2V1&nc=1&preview=1&b=123&pg=9382&template=2b",
